8649f05a091f4996b439711370d1fbb69afebfca
WUPA2 <panda.wu@oocl.com>
2019-07-19 14:28:16 +0800

    finish one counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="">
+        <button @click="add">+</button>{{number}}<button @click="decrease">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() { 
+        return {
+            number:0
+        }
+    },
+    methods:{
+        add(){
+            this.number++
+        },
+        decrease(){
+            this.number--
+        }
+    }
+ }
+</script>
+
+<style lang="" scoped>
+</style>
