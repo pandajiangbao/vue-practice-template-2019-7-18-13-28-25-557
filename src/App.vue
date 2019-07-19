56e4640f63454a66e4fb224916244ba003d13c9c
WUPA2 <panda.wu@oocl.com>
2019-07-19 14:55:49 +0800

    finish test2 ac1

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        <Counter/>
+        <input v-model.number="limit" type="text">
+        <CounterGroup :limit=limit />
     </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import Counter from "./components/Counter.vue";
+import CounterGroup from "./components/CounterGroup.vue";
 export default {
     name: "app",
+    data() { 
+        return {
+            limit:0
+        }
+    },
     components: {
         HelloWorld,
-        Counter
+        CounterGroup
     }
 };
 </script>
---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <Counter v-for="n in limit" :key="n"/>
+    </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+export default {
+    props:["limit"],
+    components:{
+        Counter
+    },
+    data() { 
+        return {
+
+        }
+    },
+    mounted(){
+    }
+ }
+</script>
+
+<style lang="" scoped>
+</style>
